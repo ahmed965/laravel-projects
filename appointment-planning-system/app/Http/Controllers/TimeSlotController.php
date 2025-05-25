098af41a22d3ed5c062a8ea19ae2e2e58812afdd
Ahmed <56020324+ahmed965@users.noreply.github.com>
2025-05-26 00:44:37 +0200

    Update TimeSlotController.php

--- appointment-planning-system/app/Http/Controllers/TimeSlotController.php
+++ appointment-planning-system/app/Http/Controllers/TimeSlotController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 use App\Models\BaseModel;
 use App\Models\TimeSlot;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class TimeSlotController extends Controller
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        return response()->json(
-            TimeSlot::where(BaseModel::IS_AVAILABLE, 1)->get()
-        );
+        $query = TimeSlot::where(BaseModel::IS_AVAILABLE, 1);
+
+        if ($request->has(BaseModel::DOCTOR_ID)) {
+            $query->where(BaseModel::DOCTOR_ID, $request->doctor_id);
+        }
+
+        return response()->json($query->get());
     }
 }
